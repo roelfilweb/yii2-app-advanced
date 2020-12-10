49a1f4bdac5552855506bde9ebaa45bf4bffb23e
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2020-12-10 09:27:55 +0800

    up

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,13 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=db_yii2advanced',
+            'username' => 'root',
+            'password' => 'password',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -1,5 +1,5 @@
 <?php
-
+//test
 $config = [
     'components' => [
         'request' => [
